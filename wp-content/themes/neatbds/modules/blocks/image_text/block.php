847abcf6285a31148b97386c48068008ee8e0215
Bruno Piorno <brunopiornop@gmail.com>
2024-08-30 14:06:58 -0300

    Create block.php

---
+++ wp-content/themes/neatbds/modules/blocks/image_text/block.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Imagen y Texto
+ *
+ * Title:       Imagen y Texto
+ * Slug:        imagen-y-texto
+ * Description: Imagen y Texto block
+ * Category:    neatbds
+ * Icon:        align-full-width
+ * Keywords:    example
+ * Post Types:  all
+ * Multiple:    true
+ * Active:      true
+ * CSS Deps:
+ * JS Deps:
+ *
+ * @package  Neatbds
+ * @subpackage defaultTheme
+ * @since defaultTheme  1.0
+ */
+
+do_action( 'neatbds_pre_render_block', $block ); 

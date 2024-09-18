fca9ae5c248c8e4778daadd297c67d530c2152e5
nahuelfc <103139615+nahuelfc@users.noreply.github.com>
2024-09-18 08:24:51 -0300

    Produccion Agricola

---
+++ wp-content/themes/neatbds/modules/blocks/paragraph/block.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Texto Enriquecido
+ *
+ * Title:       Texto Enriquecido
+ * Slug:        paragraph
+ * Description: paragraph block
+ * Category:    neatbds
+ * Icon:        align-full-width
+ * Keywords:    example
+ * Post Types:  all
+ * Multiple:    true
+ * Active:      true
+ * CSS Deps:
+ * JS Deps:
+ *
+ * @package  Neatbds
+ * @subpackage defaultTheme
+ * @since defaultTheme  1.0
+ */
+
+do_action( 'neatbds_pre_render_block', $block ); 
+$paragraph = get_field('paragraph');
+if($paragraph): ?>
+    <section class="paragraph bg--white">
+        <div class="container">
+            <div class="paragraph__cont">
+                <?php echo wp_kses_post($paragraph); ?>
+            </div>
+        </div>
+    </section>
+<?php endif; ?>

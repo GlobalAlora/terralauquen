4c6ea064f3e11cb6733afd9991fd119290e55e8f
nahuelfc <103139615+nahuelfc@users.noreply.github.com>
2024-09-30 13:41:33 -0300

    Search & servicios

---
+++ wp-content/themes/neatbds/modules/blocks/servicios/block.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Servicios
+ *
+ * Title:       Servicios
+ * Slug:        servicios
+ * Description: servicios
+ * Category:    neatbds
+ * Icon:        align-full-width
+ * Keywords:    example
+ * Post Types:  all
+ * Multiple:    true
+ * Active:      true
+ * CSS Deps:
+ * JS Deps:
+ *
+ * @package  Neatbds
+ * @subpackage defaultTheme
+ * @since defaultTheme  1.0
+ */
+
+do_action( 'neatbds_pre_render_block', $block );
+$servicios = get_field('servicios');
+if($servicios):
+?>
+
+    <section class="services bg--white">
+        <div class="container container--small">
+            <div class="services__cont bg--natural">
+                <?php foreach($servicios as $item): ?>
+                    <div class="services__item js-open-block">
+                        <?php if (!empty($item['title'])): ?>
+                            <h3 class="services__title h4"><?php echo $item['title']; ?><span class="services__icon"></span></h3>
+                        <?php endif; ?>
+                        <?php if (!empty($item['text'])): ?>
+                            <div class="services__text h5"><?php echo $item['text']; ?></div>
+                        <?php endif; ?>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </section>
+
+<?php endif;?>
--- wp-content/themes/neatbds/search.php
+++ wp-content/themes/neatbds/search.php
@@ -10,30 +10,38 @@
 get_header();
 if (have_posts()): ?>
 
-	<header class="page-header">
-		<h1 class="page-title">
-			<?php
-			/* translators: %s: search query. */
-			printf(esc_html__('Search Results for: %s', 'neatbds'), '<span>' . get_search_query() . '</span>');
-			?>
-		</h1>
+	<header class="page-header bg--natural">
+		<div class="container">
+			<h1 class="page-title">
+				<?php
+				/* translators: %s: search query. */
+				printf(esc_html__('Search Results for: %s', 'neatbds'), '<span>' . get_search_query() . '</span>');
+				?>
+			</h1>
+		</div>
 	</header><!-- .page-header -->
+	
+	<div class="search__cont bg--natural">
+		<div class="container">
+			<div class="grid-cont">
+				<?php
+				/* Start the Loop */
+				while (have_posts()):
+					the_post();
 
-	<?php
-	/* Start the Loop */
-	while (have_posts()):
-		the_post();
+					/**
+					 * Run the loop for the search to output the results.
+					 * If you want to overload this in a child theme then include a file
+					 * called content-search.php and that will be used instead.
+					 */
+					get_template_part('modules/posts/item');
 
-		/**
-		 * Run the loop for the search to output the results.
-		 * If you want to overload this in a child theme then include a file
-		 * called content-search.php and that will be used instead.
-		 */
-		get_template_part('modules/posts/item');
+				endwhile;
 
-	endwhile;
-
-	the_posts_navigation();
+				the_posts_navigation();
+			echo '</div>';
+		echo '</div>';	
+	echo '</div>';
 
 else:
 

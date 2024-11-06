2c453281b0ae1d6501de399acf104c7c308d05b0
nahuelfc <103139615+nahuelfc@users.noreply.github.com>
2024-11-06 16:41:19 -0300

    posts, search

--- wp-content/themes/neatbds/modules/posts/item.php
+++ wp-content/themes/neatbds/modules/posts/item.php
@@ -1,8 +1,10 @@
 <article class="post-item">
     <a href="<?php echo the_permalink();?>" class="latest_news__item">
-        <?php if (has_post_thumbnail()) : ?>
-            <div class="latest_news__item__image"><?php echo get_the_post_thumbnail(get_the_ID(), 'medium'); ?></div>
-        <?php endif; ?>
+        <?php 
+        $post_default_image = get_field('post_default_image', 'option');
+        $post_image = has_post_thumbnail() ? get_the_post_thumbnail(get_the_ID(), 'medium') : '<img src="' . $post_default_image['url'] . '" alt="Terralauquen">';
+        echo '<div class="latest_news__item__image">' . $post_image . '</div>';
+        ?>
         <div class="latest_news__item__cont">
             <div class="latest_news__item__date"><?php echo get_the_date(); ?></div>
             <div class="latest_news__item__title h3"><?php the_title(); ?></div>
